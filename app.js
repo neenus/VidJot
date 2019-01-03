b89cb6d6dbfdac8e07b1d8e6c9445f193b068110
Neenus Gabriel <neenus@live.ca>
2019-01-03 14:21:34 -0500

    added handlebars template engine

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
+
+// handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// ... routes ...
+// Index route 
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+// about route 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 const port = 5000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server started and listening on port ${port}`);
 });
