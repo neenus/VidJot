0f7a36055b4ed2385aefc4279ad0b50010dbad9d
Neenus Gabriel <neenus@live.ca>
2019-01-03 15:53:48 -0500

    added body-parser to handle forms input also added server side form validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -19,6 +20,10 @@ const Idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // ... routes ...
 // Index route 
 app.get('/', (req, res) => {
@@ -36,6 +41,28 @@ app.get('/about', (req, res) => {
 // Add Idea form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Process form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({text: 'Please add a title'});
+  }
+  if (!req.body.details) {
+    errors.push({text: 'Please add some details'});
+  }
+
+  if(errors.length > 0 ) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else (
+    console.log(req.body),
+    res.send('passed')
+  );
 });
 
 
