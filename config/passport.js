062459a83a7bc8d36c79ca8ae3b99ced8186a8ee
Neenus Gabriel <neenus@live.ca>
2019-01-03 23:58:56 -0500

    finished passport login authentication

--- config/passport.js
+++ config/passport.js
@@ -8,6 +8,32 @@ const User = mongoose.model('users');
 
 module.exports = function(passport) {
   passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-    console.log(email);
+    // Match user
+    User.findOne({
+      email: email
+    }).then(user => {
+      if(!user) {
+        return done(null, false, {message: 'No user found with that email'});
+      }
+      // Match passowrd
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Password does not match our records'});
+        }
+      });
+    });
   }));
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 };
