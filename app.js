4a331a765c232f0be915b5e9184de77244a136f6
Neenus Gabriel <neenus@live.ca>
2019-01-03 19:07:05 -0500

    added flash messages to CRUD actions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -27,6 +29,23 @@ app.use(bodyParser.json());
 
 // Method-Override middleware
 app.use(methodOverride('_method'));
+
+// Express sessoin middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_mesg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // ... routes ...
 // Index route 
@@ -94,6 +113,7 @@ app.post('/ideas', (req, res) => {
     new Idea(newUser)
       .save()
       .then(idea => {
+        res.req.flash('success_msg', 'Video idea added');
         res.redirect('/ideas');
       });
     }
@@ -111,6 +131,7 @@ app.put('/ideas/:id', (req, res) => {
 
     idea.save()
       .then(idea => {
+        res.req.flash('success_msg', 'Video idea edited');
         res.redirect('/ideas');
       });
   });
@@ -120,6 +141,7 @@ app.put('/ideas/:id', (req, res) => {
 app.delete('/ideas/:id', (req, res) => {
   Idea.deleteOne({_id: req.params.id})
     .then(() => {
+      res.req.flash('success_msg', 'Video idea removed');
       res.redirect('/ideas');
     });
 });
